7282d931b6e998a3533f28c8c550147f59ac8502
ShUbHaM13M <shubham.heeralal@gmail.com>
2022-11-23 09:34:35 +0530

    Initialize React Native Expo app and connection with socket.io

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
 import uvicorn
 from fastapi import FastAPI, Request
 import socketio
-from authlib.integrations.starlette_client import OAuth
-from starlette.config import Config
-from starlette.middleware.sessions import SessionMiddleware
-
-config = Config('.env')
-oauth = OAuth(config)
-oauth.register(
-	name = "google",
-	server_metadata_url = 'https://accounts.google.com/.well-known/openid-configuration',
-	client_kwargs = {
-		'scope': 'openid email profile'
-	}
-)
 
 sio = socketio.AsyncServer(async_mode="asgi")
 socket_app = socketio.ASGIApp(sio)
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="secret")
 
 @app.get('/')
 async def test():
@@ -26,8 +12,7 @@ async def test():
 
 @app.route('/login')
 async def login(request: Request):
-	redirect_uri = request.url_for('auth')
-	return await oauth.google.authorize_redirect(request, redirect_uri)
+	pass
 
 app.mount('/', socket_app)
 
@@ -40,4 +25,4 @@ async def on_disconnect(sid):
 	print(f'{sid} disconnected')
 
 if __name__ == '__main__':
-	uvicorn.run('app:app', reload=True)
+	uvicorn.run('app:app', reload=True, host="192.168.0.105", port=8000)
